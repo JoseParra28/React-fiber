0a83ffcf64aac7500dd9a64afc5cb7b213263cd3
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-28 12:03:59 +0000

    created navbar content

--- portfolio-jp/src/Components/Navbar.jsx
+++ portfolio-jp/src/Components/Navbar.jsx
@@ -1,7 +1,59 @@
+import {useEffect, useState} from "react"
+import React from "react"
+import { Link } from "react-router-dom"
+import menu from '../assets/menu.svg'
+import close from '../assets/close.svg'
+import '../Styles/App.css'
+// import { navLinks } from "../Constants"
 
+const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
 
 export const Navbar = () => {
+
+    
+    const [active, setActive] = useState("")
+    const [toggle, setToggle] = useState(false)
     return (
-        <ul>hellos</ul>
+        <>
+        <nav className="nav">
+        <div className="dropdown">
+                        <img src={toggle ? close : menu} alt="menu icon" className="hamburger-menu" onClick={() => setToggle(!toggle)} />
+                        <div className={`${!toggle ? 'hidden' : 'active'}`} id="toggle"></div>
+            </div>
+            <div className="navbar-logo">LOGO
+            </div>
+                {/* <Link 
+                to="/"
+                onClick={()=> {
+                    setActive("")
+                    window.scrollTo(0,0)}}>
+                LOGO</Link> */}
+                <div className="nav-inks">
+                    <ul>
+                    {navLinks.map((nav) => (
+                        <li key={nav.id} className={active ==  nav.title ? "active" : "unactive"} onClick={() => setActive(nav.title)}>
+                            <a href={`#${nav.id}`}>{nav.title}</a>
+                        </li>
+                    ))}
+                    </ul>
+                   
+                </div>
+            
+        </nav>
+        </>
     )
 }
+
