bed2b9ee2f53a9624972ad62f813442269ca146c
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-29 08:32:08 +0000

    added dinamic card content

--- portfolio-jp/src/Components/Card.jsx
+++ portfolio-jp/src/Components/Card.jsx
@@ -1,37 +1,63 @@
-
-const projects = [{
+import { Link, NavLink, Router } from "react-router-dom"
+import { ExternalLink } from 'react-external-link';
+const projects = [
+    {
     pic: '../assets/menu.svg',
-    title: 'Runway Models',
+    id: 'RunwayModels',
+    title: "Runway Models",
     languageOne: 'HTML',
     languageTwo: 'CSS',
     languageThree: 'Javascript',
     languageFour: 'Python',
     description: 'My very first website',
-    gitLink: '/',
+    gitLink: 'https://www.npmjs.com/package/react-external-link',
     livePreview: '/'
-}]
+},
+{
+    pic: '../assets/menu.svg',
+    id: 'RunwayModels',
+    title: "Runway Models",
+    languageOne: 'HTML',
+    languageTwo: 'CSS',
+    languageThree: 'Javascript',
+    languageFour: 'Python',
+    description: 'My very first website',
+    gitLink: 'https://github.com/JoseParra28/runway-models-final-project?tab=readme-ov-file',
+    livePreview: "https://joseparra28.github.io/runway-models-final-project/"
+}
+]
 
 export const Card = () => {
 
     return (
         <section>
-        <div className="card">
+            
+        {projects.map((project, index) => {
+            return (
+                <div key={project.id} className="card">
             <div className="image">
-                <img src={projects.pic} alt="" />image
+                <img src={project.pic} alt="" />image
             </div>
             <div className="card-content">
-                <h3>Project name </h3>
-                <span>Language</span>
-                <span>Language</span>
-                <span>Language</span>
-                <span>Language</span>
-                <p>Description</p>
+                <h3>{project.title}</h3>
+                <span>{project.languageOne}</span>
+                <span>{project.languageTwo}</span>
+                <span>{project.languageThree}</span>
+                <span>{project.languageFour}</span>
+                <p>{project.description}</p>
                 <div className="card-btns">
-                    <button>To Github</button>
-                    <button>live preview</button>
+                <ExternalLink href={project.gitLink} >
+                  <span>Gitbub</span>
+                  </ExternalLink>
+                  <ExternalLink href={project.livePreview} >
+                  <span>Live previw</span>
+                  </ExternalLink>
+                  
                 </div>
             </div>
-        </div>
+           </div>
+            )
+        })}
         </section>
     )
 }
