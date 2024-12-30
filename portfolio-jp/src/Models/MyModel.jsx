e02f712bf504d138b63d5e62466bce59ae932ba9
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-30 01:07:37 +0000

    adding animation to model

--- portfolio-jp/src/Models/MyModel.jsx
+++ portfolio-jp/src/Models/MyModel.jsx
@@ -7,28 +7,24 @@ import { TorusGeometry } from "three"
 
 export const MyModel = () => {
 
-    // const myModel = useGLTF('./desktop_pc/Portfolio-custom-model.gltf')
     const myModel = useGLTF('./myModel/model.gltf')
-    // const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
-
-
+    
     return (
         <Canvas className="about-canvas">
         <Environment preset="city"/>
         <color args={['#040404']} attach="background"/>   
         <mesh >
         
-     {/* <OrbitControls/> */}
      <PresentationControls  global rotation={[0.13, 0, 0]} >
-        <Float>
-  <primitive 
-  object={myModel.scene} position={[-28,-5,-29]} rotation-y={-2.2} scale={1.9}>
-  </primitive>
-  </Float>
-  </PresentationControls>
-  <ContactShadows  position-y={-1.4} opacity={0.4} scale={20} blur={3} color={"#aba6a6"}/>
+        <Float rotationIntensity={0.2}>
+        <rectAreaLight width={2.05} height={1.65} intensity={650} color={'#96cf24'} rotation={[ 0.5, Math.PI, 0]} position={[0,0.55, -1.15]}/> 
+          <primitive 
+          object={myModel.scene} position={[-8,-5,-1]} rotation-y={-1} scale={1.9}>
+          </primitive>
+          </Float>
+          </PresentationControls>
+          <ContactShadows  position-y={-1.4} opacity={0.4} scale={20} blur={3} color={"#aba6a6"}/>
         </mesh>
-        
     </Canvas>
 
 
