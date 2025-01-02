de1bf016d09cc360986d031e192f28cf32557999
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2025-01-02 13:07:14 +0000

    navbar

--- portfolio-jp/src/Components/Subnav.jsx
+++ portfolio-jp/src/Components/Subnav.jsx
@@ -1,6 +1,6 @@
 import logo from '../assets/logo.png'
 import menu from '../assets/menu.svg'
-import { Navbar } from './Navbar'
+// import { Navbar } from './Navbar'
 
 
 export const SubNav = () => {
